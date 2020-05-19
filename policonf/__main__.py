c2a7b756aad16fa6768bbe734a9c7b079869bc28
yazan-matar <yazan.matar@mail.polimi.it>
2020-05-19 16:20:57 +0200

    Logger writes into file

--- policonf/__main__.py
+++ policonf/__main__.py
@@ -7,9 +7,19 @@ from IPython import start_ipython
 
 def run():
     """Run the PoliCONF/IPython shell."""
-    logging.basicConfig()
+
+    # TODO: migliorare logger (leggere meglio documentazione)
+    #   Ricordarsi che ora il logger è impostato in modalità scrittura (non append)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s: \n%(message)s\n', filename='log.txt', filemode='w', level=logging.INFO)
+
     logging.getLogger().setLevel(logging.INFO)
-    logging.getLogger('policonf').setLevel(logging.DEBUG)
+    logging.getLogger('policonf').setLevel(logging.INFO)
+
+    # handler = logging.StreamHandler(sys.stdout)
+    # handler.setLevel(logging.INFO)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # handler.setFormatter(formatter)
+    # logging.getLogger().addHandler(handler)
 
     start_ipython(argv=[sys.argv[0], "-i", "-c", dedent("""
     %load_ext policonf
